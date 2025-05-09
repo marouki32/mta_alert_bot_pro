38ba51b0fa8115d419f8c6faeb5b79fd7f85a623
mootez marzouki <marzouki32@gmail.com>
2025-05-09 11:54:55 +0100

    ci: add pages.yml workflow, init DB, generate dashboard

--- gui/tabs/chart_tab.py
+++ gui/tabs/chart_tab.py
@@ -3,6 +3,8 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import mplcursors
+
 from api.api import get_ohlcv
 from analysis.technical_analysis import compute_indicators
 
@@ -26,7 +28,7 @@ class ChartTab(QWidget):
         self.canvas = FigureCanvas(self.fig)
         layout.addWidget(self.canvas)
 
-        # Connexion du changement de symbole
+        # Quand on change de symbole, on redessine
         self.combo.currentTextChanged.connect(self.on_symbol_change)
 
         # Tracé initial
@@ -43,14 +45,23 @@ class ChartTab(QWidget):
         self.fig.clear()
         ax = self.fig.add_subplot(111)
         ax.plot(df.index, df['close'], label='Close')
+
         # tracer toutes les EMA
         for col in df.columns:
             if col.startswith('ema_'):
                 ax.plot(df.index, df[col], label=col)
+
         # tracer Bollinger si présent
-        if 'bb_upper' in df and 'bb_lower' in df:
+        if 'bb_upper' in df.columns and 'bb_lower' in df.columns:
             ax.plot(df.index, df['bb_upper'], '--', label='BB upper')
             ax.plot(df.index, df['bb_lower'], '--', label='BB lower')
+
         ax.set_title(symbol)
         ax.legend()
+        ax.grid(True)
+
+        # 4) Activer l’infobulle interactive au survol
+        mplcursors.cursor(ax.lines, hover=True)
+
+        # 5) Rafraîchir l’affichage
         self.canvas.draw()
